93b11ef22a1e65f9c1234a400a65b1dcb253dd4c
Morten Poul Jensen <mpj@signifly.com>
2018-01-29 16:27:13 +0100

    Add order resource

---
+++ src/Resources/OrderResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Signifly\Shopify\Resources;
+
+class OrderResource extends ApiResource
+{
+    public function delete()
+    {
+        return $this->shopify->orders()->destroy($this->id);
+    }
+
+    public function update(array $data)
+    {
+        return $this->shopify->orders()->update($this->id, $data);
+    }
+
+    public function cancel()
+    {
+        return $this->shopify->orders()->cancel($this->id);
+    }
+
+    public function close()
+    {
+        return $this->shopify->orders()->close($this->id);
+    }
+
+    public function open()
+    {
+        return $this->shopify->orders()->open($this->id);
+    }
+}
