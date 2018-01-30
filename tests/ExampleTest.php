45c6f69414758d62682b78953a89f5bd53e453f7
Travis Elkins <travis.elkins+github@gmail.com>
2018-01-30 12:52:31 +0100

    Minor CS update to existing test.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -10,7 +10,7 @@ class ExampleTest extends TestCase
      * @test
      * @group integration
      */
-    function it_can_reach_the_shopify_api()
+    public function it_can_reach_the_shopify_api()
     {
         $shopify = new Shopify(env('SHOPIFY_API_KEY'), env('SHOPIFY_API_SECRET'), env('SHOPIFY_HANDLE'));
 
