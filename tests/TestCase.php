6546086a2e3c6c7a5c88c25cba6ff31e1a2bbae7
Travis Elkins <travis.elkins+github@gmail.com>
2018-01-30 15:55:18 +0100

    Moved mock guzzle functionality into base test case...for now, moved response json into fixture file, added simple fixture access, introduced guzzle history and added a couple simple assertions to demonstrate use.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,36 @@
 
 namespace Signifly\Shopify\Test;
 
+use GuzzleHttp\Middleware;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\MockHandler;
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Signifly\Shopify\Profiles\GuzzleMockHandlerProfile;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected $mockHistory = null;
+
+    protected function getFixture(string $filename)
+    {
+        return file_get_contents(__DIR__ . '//fixtures//' . $filename);
+    }
+
+    protected function makeGuzzleResponse(int $status = 200, array $headers = [],
+        string $body = null)
+    {
+        return new Response($status, $headers, $body);
+    }
+
+    protected function makeGuzzleMockHandlerProfile(array $queue = null)
+    {
+        $this->mockHistory = [];
+        $history = Middleware::history($this->mockHistory);
+
+        $stack = HandlerStack::create(new MockHandler($queue));
+        $stack->push($history);
+
+        return new GuzzleMockHandlerProfile($stack);
+    }
 }
