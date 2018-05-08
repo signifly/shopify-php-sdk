8b0d613787bda405a085fe6b39c328fcf334dbf5
Morten Poul Jensen <mpj@signifly.com>
2018-05-08 16:03:40 +0200

    Make sure it returns a resource

--- src/Actions/OrderAction.php
+++ src/Actions/OrderAction.php
@@ -6,16 +6,28 @@ class OrderAction extends Action
 {
     public function cancel($id)
     {
-        return $this->shopify->post($this->path($id)->appends('cancel'));
+        $this->guardAgainstMissingParent('cancel');
+
+        $response = $this->shopify->post($this->path($id)->appends('cancel'));
+
+        return $this->transformItemFromResponse($response);
     }
 
     public function close($id)
     {
-        return $this->shopify->post($this->path($id)->appends('close'));
+        $this->guardAgainstMissingParent('close');
+
+        $response = $this->shopify->post($this->path($id)->appends('close'));
+
+        return $this->transformItemFromResponse($response);
     }
 
     public function open($id)
     {
-        return $this->shopify->post($this->path($id)->('open'));
+        $this->guardAgainstMissingParent('open');
+
+        $response = $this->shopify->post($this->path($id)->appends('open'));
+
+        return $this->transformItemFromResponse($response);
     }
 }
