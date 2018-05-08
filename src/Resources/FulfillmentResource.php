346d85fe3dd23ff48d81e44741803bd92976a98c
Morten Poul Jensen <mpj@signifly.com>
2018-05-08 14:55:02 +0200

    Add cancel, complete and open methods to fulfillment resource

--- src/Resources/FulfillmentResource.php
+++ src/Resources/FulfillmentResource.php
@@ -4,5 +4,18 @@ namespace Signifly\Shopify\Resources;
 
 class FulfillmentResource extends ApiResource
 {
+    public function cancel()
+    {
+        return $this->shopify->orderFulfillments($this->order_id)->cancel($this->id);
+    }
 
+    public function complete()
+    {
+        return $this->shopify->orderFulfillments($this->order_id)->complete($this->id);
+    }
+
+    public function open()
+    {
+        return $this->shopify->orderFulfillments($this->order_id)->open($this->id);
+    }
 }
